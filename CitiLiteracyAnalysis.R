6d44d42a5823e73e7dd172cd394b17318ec6d886
senthileie@gmail.com <senthil.veluswamy@sqs.com>
2016-12-21 20:05:15 +0000

    scatter plots added +final file uploaded

--- CitiLiteracyAnalysis.R
+++ CitiLiteracyAnalysis.R
@@ -20,30 +20,39 @@ InFile$X0.6_population_male<-as.integer(InFile$X0.6_population_male)
 write.csv(InFile,file="CleanFile.csv")
 
 #statistical analysis
-names(InFile)
-mean(InFile$X0.6_population_total)
-min(InFile$X0.6_population_total)
-range(InFile$X0.6_population_total)
-diff(range(InFile$X0.6_population_total))
-quantile(InFile$X0.6_population_total)
-var(InFile$X0.6_population_total)
-sd(InFile$X0.6_population_total)
 install.packages("moments")
 library(moments)
-skewness(InFile$X0.6_population_total)
-kurtosis(InFile$X0.6_population_total)
-plot(density(InFile$X0.6_population_total))
-summary(InFile$X0.6_population_total)
 
 # check the dependancy between the "number of graduates" and the male population and the female population
 # The below covriance shows that there is a positive relation between the two parameters on the "number of graduates"  
 cov(InFile$population_female,InFile$total_graduates)
 cov(InFile$population_male,InFile$total_graduates)
 cov(InFile$population_total,InFile$total_graduates)
+cov(InFile$literates_female,InFile$total_graduates)
+cov(InFile$literates_male,InFile$total_graduates)
 
-# correlation coefficient ( this is check how much close the relation ship is ) 
+
+# correlation coefficient ( this is to check how close is the relationship )
+# if the relation is close enough then attempt will be made to create a regression model 
 cor(InFile$population_female,InFile$total_graduates)
 cor(InFile$population_male,InFile$total_graduates)
-cor(InFile$literates_female,InFile$total_graduates)
 cor(InFile$population_total,InFile$total_graduates)
+cor(InFile$literates_female,InFile$total_graduates)
 cor(InFile$literates_male,InFile$total_graduates)
+
+plot(InFile$population_female ~ InFile$total_graduates) # the plot seems to show a linear relationship
+plot(InFile$population_male ~ InFile$total_graduates)  # the plot seems to show a linear relationship
+plot(InFile$literates_female ~ InFile$total_graduates) # the plot seems to show a linear relationship
+plot(InFile$population_total ~ InFile$total_graduates)  # the plot doesnt show any relation which is meaningful enough to make a relation
+plot(InFile$literates_male ~ InFile$total_graduates) # the plot seems to show a linear relationship
+
+#Linear model to with details from women
+lmodel<-lm(InFile$total_graduates ~ InFile$population_female+InFile$literates_female, data = InFile )
+summary(lmodel)
+
+#Linear model to with details from men
+lmodel<-lm(InFile$total_graduates ~ InFile$population_male+InFile$literates_male, data = InFile )
+summary(lmodel)
+
+#scatterplot to see the relation between literacy rate of male and female
+plot(InFile$effective_literacy_rate_male,InFile$effective_literacy_rate_female,col=c("red","green","blue","yellow"))
