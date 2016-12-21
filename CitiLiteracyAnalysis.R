a517a340dc502a25eb93b7c7add67d8cc10d3e33
senthileie@gmail.com <senthil.veluswamy@sqs.com>
2016-12-21 15:31:18 +0000

    added more

--- CitiLiteracyAnalysis.R
+++ CitiLiteracyAnalysis.R
@@ -16,5 +16,34 @@ sum(is.na(InFile))
 InFile$population_total<-as.integer(InFile$population_total)
 InFile$X0.6_population_total<-as.integer(InFile$X0.6_population_total)
 InFile$X0.6_population_male<-as.integer(InFile$X0.6_population_male)
+#write clean file into a new CSV
 write.csv(InFile,file="CleanFile.csv")
 
+#statistical analysis
+names(InFile)
+mean(InFile$X0.6_population_total)
+min(InFile$X0.6_population_total)
+range(InFile$X0.6_population_total)
+diff(range(InFile$X0.6_population_total))
+quantile(InFile$X0.6_population_total)
+var(InFile$X0.6_population_total)
+sd(InFile$X0.6_population_total)
+install.packages("moments")
+library(moments)
+skewness(InFile$X0.6_population_total)
+kurtosis(InFile$X0.6_population_total)
+plot(density(InFile$X0.6_population_total))
+summary(InFile$X0.6_population_total)
+
+# check the dependancy between the "number of graduates" and the male population and the female population
+# The below covriance shows that there is a positive relation between the two parameters on the "number of graduates"  
+cov(InFile$population_female,InFile$total_graduates)
+cov(InFile$population_male,InFile$total_graduates)
+cov(InFile$population_total,InFile$total_graduates)
+
+# correlation coefficient ( this is check how much close the relation ship is ) 
+cor(InFile$population_female,InFile$total_graduates)
+cor(InFile$population_male,InFile$total_graduates)
+cor(InFile$literates_female,InFile$total_graduates)
+cor(InFile$population_total,InFile$total_graduates)
+cor(InFile$literates_male,InFile$total_graduates)
