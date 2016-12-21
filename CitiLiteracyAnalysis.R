914eef1de79169248d024f346fc21f9389d7dc7f
senthileie@gmail.com <senthil.veluswamy@sqs.com>
2016-12-21 11:16:43 +0000

    the program not remmoves the rows with "blank space" and "*"

--- CitiLiteracyAnalysis.R
+++ CitiLiteracyAnalysis.R
@@ -1,15 +1,20 @@
 #Read the dataset file
 InFile<-read.csv(file="C:/Users/VeluswamyS/Documents/GitHub/DATASET/cities_r2.csv",header = TRUE, strip.white = TRUE,sep = ",")
-names(InFile)
+#names(InFile)
+#check the rows with Junk characters values
+InFile=subset(InFile, InFile$population_total!="*")
+InFile=subset(InFile, InFile$X0.6_population_total!="*")
+#write.csv(InFile,file="TESTFILE2.csv")
+
 #check the rows with missing values
 sum(is.na(InFile))
 #remove rows with missing values
 InFile<-na.omit(InFile)
 #check the rows with missing 
 sum(is.na(InFile))
-#clean up junk characters
-class(InFile$population_total)
-population_total<-as.character(InFile$population_total)
-class(population_total)
-population_total<-sub("*","",InFile)
+#converting factors into intergers
 InFile$population_total<-as.integer(InFile$population_total)
+InFile$X0.6_population_total<-as.integer(InFile$X0.6_population_total)
+InFile$X0.6_population_male<-as.integer(InFile$X0.6_population_male)
+write.csv(InFile,file="CleanFile.csv")
+
